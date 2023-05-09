b194e2e518b5c826c28d69a2fb0cbab4484fcbd9
Anakin <oven4151@gmail.com>
2023-05-09 13:43:45 +0700

    Back to vue 2

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -6,8 +10,15 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
+
 new Vue({
   router,
   store,
   render: (h) => h(App),
+  vuetify: new Vuetify({
+    icons: {
+      iconfont: "mdi",
+    },
+  }),
 }).$mount("#app");
